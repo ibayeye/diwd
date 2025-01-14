988c48870acf2fe84266e8efaecaa31c6e326837
Muhamad Iqbal Ramadan <169161293+ibayeye@users.noreply.github.com>
2025-01-14 11:59:15 +0700

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const db = new Sequelize({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: "bughchgyl1r5xzbepdit-mysql.services.clever-cloud.com",
+  username: "ufdfqkspjtqyhd6a",
+  password: "87410fRim7dTuvdqLrM3",
+  database: "bughchgyl1r5xzbepdit",
+  port: "3306",
   dialect: "mysql",
 });
 
