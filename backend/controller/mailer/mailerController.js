fa7e0122346a781568d05954a8269beed84d60ae
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-01-14 21:38:48 +0700

    Update mailerController.js

--- backend/controller/mailer/mailerController.js
+++ backend/controller/mailer/mailerController.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import asyncHandler from "../../middleware/asyncHandler.js";
 
 const transporter = nodemailer.createTransport({
     secure: true,
@@ -11,16 +12,29 @@ const transporter = nodemailer.createTransport({
 })
 
 
-export const sendMail = (from, to, sub, msg) => {
-    console.log(from, to, sub, msg);
+export const sendMail = asyncHandler(async (req, res) => {
     
-    transporter.sendMail({
-        from: from,
-        to: to,
-        subject: sub,
-        html: msg
-    })
-}
+    try {
+        await transporter.sendMail({
+            from: 'skripsidiwd@gmail.com',
+            to: 'iqbal.gitlab@gmail.com', 
+            subject: 'Pemberitahuan Perangkat', 
+            html: 'testing'
+            // html: '<h1>Pemberitahuan Perangkat</h1><p>Perangkat dengan ID: <strong>${deviceId}</strong></p><p>onsitevalue: ${onsitevalue}</p><p>regvalue: ${regvalue}</p>'
+        });
+
+        res.status(200).json({
+            status: "success",
+            msg: "Email sent successfully!"
+        });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({
+            status: "error",
+            msg: "Failed to send email. Please try again later."
+        });
+    }
+});
 // function sendMail(from, to, sub, msg) {
 //     console.log(from, to, sub, msg);
     
@@ -31,8 +45,8 @@ export const sendMail = (from, to, sub, msg) => {
 //         html: msg
 //     })
 // }
+// sendMail('skripsidiwd@gmail.com' ,'iqbal.gitlab@gmail.com','asd','testing')
 
-sendMail('skripsidiwd@gmail.com' ,'iqbal.gitlab@gmail.com','asd','testing')
 
 
 // Fungsi untuk mengirim email
