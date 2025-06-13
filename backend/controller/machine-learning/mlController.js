08367c56d205df6179e9c1cd752031efd3bcd0de
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-06-13 15:52:33 +0700

    up

--- backend/controller/machine-learning/mlController.js
+++ backend/controller/machine-learning/mlController.js
@@ -130,3 +130,88 @@ export const getTopErrorPerStatus = asyncHandler(async (req, res) => {
         data: data
     });
 })
+
+export const getSavedCleanedDataSet = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/save-cleaned-dataset');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+
+export const getSelectedDataset = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/selected-dataset');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+
+export const getSmoteDistribution = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/smote-distribution');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+export const getTrainModels = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/train-models');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+export const getLoadSvmModel = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/load-svm-models');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+export const getEvaluateModels = asyncHandler(async (req, res) => {
+    const response = await axios.get('http://ml-service:5001/api/evaluate-models');
+    const data = response.data
+
+    res.status(200).json({
+        status: "success",
+        data: data
+    });
+})
+export const getPredictStatus = asyncHandler(async (req, res) => {
+    try {
+        // Ambil data dari request body yang dikirim ke Express
+        const requestData = req.body;
+
+        console.log('Data yang diterima Express:', requestData); // Debug
+
+        // Kirim data ke Python endpoint
+        const response = await axios.post('http://ml-service:5001/api/predict-status', requestData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = response.data;
+
+        res.status(200).json({
+            status: "success",
+            data: data
+        });
+    } catch (error) {
+        console.error('Error:', error.response?.data || error.message);
+        res.status(500).json({
+            status: "error",
+            message: error.response?.data?.error || error.message
+        });
+    }
+});
