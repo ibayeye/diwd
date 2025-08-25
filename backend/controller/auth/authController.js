794f6390456cd45fed3048af05a95af4f62a0dc6
ibayeye <iqbal.gitlab@gmail.com>
2025-08-25 08:13:55 +0700

    Update authController.js

--- backend/controller/auth/authController.js
+++ backend/controller/auth/authController.js
@@ -26,6 +26,17 @@ export const register = asyncHandler(async (req, res) => {
     });
   }
 
+  if (!password || password.length < 6) {
+    console.log("password:", password, "len:", password?.length);
+    console.log("confirmPassword:", confirmPassword, "len:", confirmPassword?.length);
+
+    return res.status(400).json({
+      status: "error",
+      msg: "Password must be at least 6 characters",
+    });
+
+  }
+
   if (password !== confirmPassword) {
     return res.status(400).json({
       status: "error",
@@ -113,50 +124,50 @@ export const addPengguna = asyncHandler(async (req, res) => {
 
 export const login = asyncHandler(async (req, res) => {
 
-    const { username, password, keepLogin } = req.body;
+  const { username, password, keepLogin } = req.body;
 
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({ status: "error", msg: "username and password are required" });
-    }
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ status: "error", msg: "username and password are required" });
+  }
 
-    const pengguna = await Pengguna.findOne({ where: { username } });
-    if (!pengguna || !(await pengguna.comparePassword(password))) {
-        return res
-            .status(400)
-            .json({ status: "error", msg: "Invalid username or password" });
-    }
-    const token = jwt.sign({ id: pengguna.id }, process.env.JWT_SECRET, {
-        expiresIn: keepLogin ? "30d" : "6d",
-    });
-    pengguna.activeSession = token;
-    await pengguna.save();
+  const pengguna = await Pengguna.findOne({ where: { username } });
+  if (!pengguna || !(await pengguna.comparePassword(password))) {
+    return res
+      .status(400)
+      .json({ status: "error", msg: "Invalid username or password" });
+  }
+  const token = jwt.sign({ id: pengguna.id }, process.env.JWT_SECRET, {
+    expiresIn: keepLogin ? "30d" : "6d",
+  });
+  pengguna.activeSession = token;
+  await pengguna.save();
 
-    // res.cookie("jwt", token, cookieOptions);
-    res.cookie("token", token, {
-        httpOnly: true, // Membuat cookie tidak bisa diakses dari JavaScript
-        secure: true, // Cookie hanya dikirim melalui HTTPS
-        sameSite: "strict", // Melindungi dari CSRF
-        maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie berlaku selama 7 hari
-    });
+  // res.cookie("jwt", token, cookieOptions);
+  res.cookie("token", token, {
+    httpOnly: true, // Membuat cookie tidak bisa diakses dari JavaScript
+    secure: true, // Cookie hanya dikirim melalui HTTPS
+    sameSite: "strict", // Melindungi dari CSRF
+    maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie berlaku selama 7 hari
+  });
 
-    res.status(200).json({
-        status: "ok",
-        msg: "Successful login",
-        data: {
-            token: token,
-            id: pengguna.id,
-            username: pengguna.username, // Ganti dengan properti nama pengguna Anda dari database
-            email: pengguna.email,
-            role: pengguna.role,
-            nama: pengguna.nama,
-            nip: pengguna.nip,
-            no_hp: pengguna.no_hp,
-            address: pengguna.address,
-            image: pengguna.image
-        },
-    });
+  res.status(200).json({
+    status: "ok",
+    msg: "Successful login",
+    data: {
+      token: token,
+      id: pengguna.id,
+      username: pengguna.username, // Ganti dengan properti nama pengguna Anda dari database
+      email: pengguna.email,
+      role: pengguna.role,
+      nama: pengguna.nama,
+      nip: pengguna.nip,
+      no_hp: pengguna.no_hp,
+      address: pengguna.address,
+      image: pengguna.image
+    },
+  });
 });
 
 export const logout = asyncHandler(async (req, res) => {
@@ -200,7 +211,7 @@ export const getPengguna = asyncHandler(async (req, res) => {
 
   const pengguna = await Pengguna.findOne({
     where: { id },
-    attributes: ["id", "username", "image","email", "nama", "nip", "no_hp", "role"],
+    attributes: ["id", "username", "image", "email", "nama", "nip", "no_hp", "role"],
   });
 
   if (!pengguna) {
@@ -278,87 +289,87 @@ export const deletePengguna = asyncHandler(async (req, res) => {
 });
 
 export const updatePengguna = asyncHandler(async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    if (!id) {
-        return res.status(400).json({
-            status: "error",
-            msg: "Pengguna ID is required",
-        });
-    }
-
-    const pengguna = await Pengguna.findOne({ where: { id } });
-
-    if (!pengguna) {
-        return res.status(404).json({
-            status: "error",
-            msg: "Pengguna not found",
-        });
-    }
-
-    const { nama, email, no_hp, nip, image, password, isActive, address, role } = req.body;
-
-    const updatedData = {};
-    if (nama !== undefined) updatedData.nama = nama;
-    if (email !== undefined) updatedData.email = email;
-    if (no_hp !== undefined) updatedData.no_hp = no_hp;
-    if (nip !== undefined) updatedData.nip = nip;
-    if (image !== undefined) updatedData.image = image;
-    if (isActive !== undefined) updatedData.isActive = isActive;
-    if (address !== undefined) updatedData.address = address;
-    if (role !== undefined) updatedData.role = role;
-
-    if (password) {
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-        if (!passwordRegex.test(password)) {
-            return res.status(400).json({
-                status: "error",
-                msg: "Password harus minimal 6 karakter, mengandung huruf dan angka",
-            });
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        updatedData.password = hashedPassword;
-    }
-
-    if (Object.keys(updatedData).length === 0) {
-        return res.status(400).json({
-            status: "error",
-            msg: "No valid fields provided for update.",
-        });
-    }
-
-    await pengguna.update(updatedData);
-
-    const updatedPengguna = await Pengguna.findByPk(id);
-
-    // Hapus password dari respon
-    const penggunaWithoutPassword = { ...updatedPengguna.toJSON() };
-    delete penggunaWithoutPassword.password;
-
-    res.status(200).json({
-        status: "success",
-        msg: "Berhasil ubah data akun!",
-        data: penggunaWithoutPassword,
+  if (!id) {
+    return res.status(400).json({
+      status: "error",
+      msg: "Pengguna ID is required",
     });
+  }
+
+  const pengguna = await Pengguna.findOne({ where: { id } });
+
+  if (!pengguna) {
+    return res.status(404).json({
+      status: "error",
+      msg: "Pengguna not found",
+    });
+  }
+
+  const { nama, email, no_hp, nip, image, password, isActive, address, role } = req.body;
+
+  const updatedData = {};
+  if (nama !== undefined) updatedData.nama = nama;
+  if (email !== undefined) updatedData.email = email;
+  if (no_hp !== undefined) updatedData.no_hp = no_hp;
+  if (nip !== undefined) updatedData.nip = nip;
+  if (image !== undefined) updatedData.image = image;
+  if (isActive !== undefined) updatedData.isActive = isActive;
+  if (address !== undefined) updatedData.address = address;
+  if (role !== undefined) updatedData.role = role;
+
+  if (password) {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        status: "error",
+        msg: "Password harus minimal 6 karakter, mengandung huruf dan angka",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    updatedData.password = hashedPassword;
+  }
+
+  if (Object.keys(updatedData).length === 0) {
+    return res.status(400).json({
+      status: "error",
+      msg: "No valid fields provided for update.",
+    });
+  }
+
+  await pengguna.update(updatedData);
+
+  const updatedPengguna = await Pengguna.findByPk(id);
+
+  // Hapus password dari respon
+  const penggunaWithoutPassword = { ...updatedPengguna.toJSON() };
+  delete penggunaWithoutPassword.password;
+
+  res.status(200).json({
+    status: "success",
+    msg: "Berhasil ubah data akun!",
+    data: penggunaWithoutPassword,
+  });
 });
 
 export const userLoggedin = asyncHandler(async (req, res) => {
-    const pengguna = await Pengguna.findOne({
-        where: { id: req.user.id },
-        attributes: ["id", "username", "image", "email", "nama", "nip", "no_hp", "role", "isActive", "address"],
-    });
+  const pengguna = await Pengguna.findOne({
+    where: { id: req.user.id },
+    attributes: ["id", "username", "image", "email", "nama", "nip", "no_hp", "role", "isActive", "address"],
+  });
 
-    if (!pengguna) {
-        return res.status(404).json({
-            status: "error",
-            msg: "Pengguna not found",
-        });
-    }
-
-    res.status(200).json({
-        status: "success",
-        msg: "Pengguna data retrieved successfully",
-        data: pengguna,
+  if (!pengguna) {
+    return res.status(404).json({
+      status: "error",
+      msg: "Pengguna not found",
     });
+  }
+
+  res.status(200).json({
+    status: "success",
+    msg: "Pengguna data retrieved successfully",
+    data: pengguna,
+  });
 });
